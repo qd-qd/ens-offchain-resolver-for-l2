30443270d5e0e4ce7ce939fa5f7f99ff3a05382f
qdqd <qdqdqdqdqd@protonmail.com>
2022-04-21 21:44:41 +0200

    refacto: adapt the offchain resolver
    - Remove ops files
    - Use dotenv to set the env variables as default option values

--- packages/contracts/deploy/10_offchain_resolver.js
+++ packages/contracts/deploy/10_offchain_resolver.js
@@ -1,15 +1,21 @@
 const { ethers } = require("hardhat");
 
-module.exports = async ({getNamedAccounts, deployments, network}) => {
-    const {deploy} = deployments;
-    const {deployer, owner} = await getNamedAccounts();
-    if(!network.config.gatewayurl){
-        throw("gatewayurl is missing on hardhat.config.js");
-    }
-    await deploy('OffchainResolver', {
-        from: deployer,
-        args: [network.config.gatewayurl, [owner]],
-        log: true,
-    });
+// @DEV paste here the address printed by `yarn start:gateway`
+const owner = "0x83EAD75c0191c3184CaAd30FF8061e89f9d851a1";
+
+module.exports = async ({ getNamedAccounts, deployments, network }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  if (!network.config.gatewayurl) {
+    throw "gatewayurl is missing on hardhat.config.js";
+  }
+
+  await deploy("OffchainResolver", {
+    from: deployer,
+    args: [network.config.gatewayurl, [owner]],
+    log: true,
+  });
 };
-module.exports.tags = ['test', 'demo'];
+
+module.exports.tags = ["test", "demo"];
--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -3,15 +3,26 @@ import { Command } from 'commander';
 import { readFileSync } from 'fs';
 import { ethers } from 'ethers';
 import { JSONDatabase } from './json';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env.local' });
+
 const program = new Command();
+
 program
-  .requiredOption(
+  .option(
     '-k --private-key <key>',
-    'Private key to sign responses with. Prefix with @ to read from a file'
+    'Private key to sign responses with. Prefix with @ to read from a file',
+    process.env.PRIVATE_KEY
   )
-  .requiredOption('-d --data <file>', 'JSON file to read data from')
-  .option('-t --ttl <number>', 'TTL for signatures', '300')
-  .option('-p --port <number>', 'Port number to serve on', '8080');
+  .option('-d --data <file>', 'JSON file to read data from', process.env.DATA)
+  .option('-t --ttl <number>', 'TTL for signatures', process.env.TTL || '300')
+  .option(
+    '-p --port <number>',
+    'Port number to serve on',
+    process.env.PORT || '8080'
+  );
+
 program.parse(process.argv);
 const options = program.opts();
 let privateKey = options.privateKey;
@@ -24,6 +35,7 @@ const address = ethers.utils.computeAddress(privateKey);
 const signer = new ethers.utils.SigningKey(privateKey);
 const db = JSONDatabase.fromFilename(options.data, parseInt(options.ttl));
 const app = makeApp(signer, '/', db);
+
 console.log(`Serving on port ${options.port} with signing address ${address}`);
 app.listen(parseInt(options.port));
 
